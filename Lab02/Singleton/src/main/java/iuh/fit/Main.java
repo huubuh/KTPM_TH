380f31a67e3428bccd09fdf19329606c35412cb3
Huy Nguyen <luonghoc1011@gmail.com>
2026-03-14 19:52:33 +0700

    Tuan02

---
+++ Lab02/Singleton/src/main/java/iuh/fit/Main.java
@@ -0,0 +1,16 @@
+package iuh.fit;
+
+import iuh.fit.Singleton.Singleton;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Singleton obj1 = Singleton.getInstance();
+        Singleton obj2 = Singleton.getInstance();
+
+        obj1.showMessage();
+
+        System.out.println(obj1 == obj2); // true
+    }
+}
